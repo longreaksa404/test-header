fc77fa593aa36caa43d3d77529ab1f72852ff205
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-02-02 23:20:28 +0700

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <AppHeader />
+    <main class="mt-24 p-8 text-center">
+      <h1 class="text-4xl font-bold">Welcome to the Test</h1>
+      <p class="mt-4 text-gray-600">Nuxt 3 + Tailwind + ISR is running!</p>
+    </main>
+  </div>
+</template>
